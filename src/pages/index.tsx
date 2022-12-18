44297e62b15cd404eb3346dac7e34d73634aa5ff
Hayden Young <hayden@hbjy.dev>
2022-12-18 14:21:35 +0000

    lint: move classes onto newlines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,37 @@ const Home: NextPage = () => {
         </h3>
 
         <div className="flex items-center gap-6">
-          <Link href="/posts" className="rounded-lg bg-fuchsia-600 px-6 py-3 text-white shadow-lg shadow-fuchsia-500 transition-shadow duration-200 hover:shadow-xl hover:shadow-fuchsia-500">
+          <Link
+            href="/posts"
+            className="
+              rounded-lg
+              bg-fuchsia-600
+              px-6
+              py-3
+              text-white
+              shadow-lg
+              shadow-fuchsia-500
+              transition-shadow
+              duration-200
+              hover:shadow-xl
+              hover:shadow-fuchsia-500
+            "
+          >
             Read what I have to say
           </Link>
 
-          <button className="rounded-lg px-6 py-3 text-zinc-700 shadow-lg transition-shadow duration-200 hover:shadow-xl">
+          <button
+            className="
+              rounded-lg
+              px-6
+              py-3
+              text-zinc-700
+              shadow-lg
+              transition-shadow
+              duration-200
+              hover:shadow-xl
+            "
+          >
             Look at my About page
           </button>
         </div>
